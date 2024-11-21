e9a3649ae7b95e31ee782704a66dedcc1fbddb45
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-21 16:06:43 +0500

    settings and launch screen

---
+++ SwiftUICrypto/Core/Launch/Views/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  SwiftUICrypto
+//
+//  Created by Adkhamjon Rakhimov on 21/11/24.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
